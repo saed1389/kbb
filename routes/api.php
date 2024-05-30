957cf95765a94860c18fa4e235d5199197847596
saed1389 <saed.1389@gmail.com>
2024-05-30 21:13:43 +0300

    Api Auth

--- routes/api.php
+++ routes/api.php
@@ -19,25 +19,25 @@ use Illuminate\Support\Facades\Route;
 Route::post('login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    // Fetch all news
+    Route::get('allNews', [NewsController::class, 'index']);
+    Route::get('sliders', [NewsController::class, 'slider']);
+    Route::get('news-detail/{id}', [NewsController::class, 'show']);
+    Route::get('events', [NewsController::class, 'events']);
+    Route::get('event-detail/{id}', [NewsController::class, 'eventDetail']);
+    Route::get('chatUser', [NewsController::class, 'chatUser']);
+    Route::get('profile', [NewsController::class, 'profile']);
+    Route::post('profileStore', [NewsController::class, 'profileStore']);
+    Route::post('kbbSchool', [NewsController::class, 'kbbSchool']);
+    Route::post('kbbCompetenceStore', [NewsController::class, 'kbbCompetenceStore']);
+    Route::get('kbbCompetenceList', [NewsController::class, 'kbbCompetenceList']);
+    Route::get('kbbCompetencePoint', [NewsController::class, 'kbbCompetencePoint']);
+    Route::get('kbbCompetenceTotal', [NewsController::class, 'kbbCompetenceTotal']);
+    Route::get('notification', [NewsController::class, 'notification']);
+
+    Route::post("SendMessage", [NewsController::class, "SendMessage"]);
+    Route::get("load", [NewsController::class, "LoadThePreviousMessages"]);
 });
 
-Route::get('allNews', [NewsController::class, 'index']);
-Route::get('sliders', [NewsController::class, 'slider']);
-Route::get('news-detail/{id}', [NewsController::class, 'show']);
-Route::get('events', [NewsController::class, 'events']);
-Route::get('event-detail/{id}', [NewsController::class, 'eventDetail']);
-Route::get('chatUser', [NewsController::class, 'chatUser']);
-Route::get('profile', [NewsController::class, 'profile']);
-Route::post('profileStore', [NewsController::class, 'profileStore']);
-Route::post('kbbSchool', [NewsController::class, 'kbbSchool']);
-Route::post('kbbCompetenceStore', [NewsController::class, 'kbbCompetenceStore']);
-Route::get('kbbCompetenceList', [NewsController::class, 'kbbCompetenceList']);
-Route::get('kbbCompetencePoint', [NewsController::class, 'kbbCompetencePoint']);
-Route::get('kbbCompetenceTotal', [NewsController::class, 'kbbCompetenceTotal']);
-Route::get('notification', [NewsController::class, 'notification']);
 
-Route::post("SendMessage", [NewsController::class, "SendMessage"]);
-Route::get("load", [NewsController::class, "LoadThePreviousMessages"]);
 
 
